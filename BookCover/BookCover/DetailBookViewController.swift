4fc199fd054d115d7852a94454a9c4393550c76b
carlos-santiago-2017 <c.santiago.cruz@gmail.com>
2018-12-17 16:29:25 -0600

    Add a DetailBookViewController, then custom the subclass. After this, create a segue from the cell to the UI viewcontroller

---
+++ BookCover/BookCover/DetailBookViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailBookViewController.swift
+//  BookCover
+//
+//  Created by Carlos Santiago Cruz on 12/17/18.
+//  Copyright © 2018 Carlos Santiago Cruz. All rights reserved.
+//
+
+import UIKit
+
+class DetailBookViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
