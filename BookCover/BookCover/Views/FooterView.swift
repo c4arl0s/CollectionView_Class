b7ef8e75649e0ad377fd55e90531400d49dc13ac
carlos-santiago-2017 <c.santiago.cruz@gmail.com>
2019-01-04 15:56:46 -0600

    Create a subclass for the footer view

---
+++ BookCover/BookCover/Views/FooterView.swift
@@ -0,0 +1,13 @@
+//
+//  FooterView.swift
+//  BookCover
+//
+//  Created by Carlos Santiago Cruz on 1/4/19.
+//  Copyright © 2019 Carlos Santiago Cruz. All rights reserved.
+//
+
+import UIKit
+
+class FooterView: UICollectionReusableView {
+        
+}
