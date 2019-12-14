cbfe036953772aee8425ae39913af4203574a25c
c4arl0s <c.santiago.cruz@gmail.com>
2019-12-14 16:29:19 -0600

    add new project

---
+++ List1SubclassCollectionViewCell/List1SubclassCollectionViewCell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  List1SubclassCollectionViewCell
+//
+//  Created by Carlos Santiago Cruz on 06/12/19.
+//  Copyright © 2019 Carlos Santiago Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
